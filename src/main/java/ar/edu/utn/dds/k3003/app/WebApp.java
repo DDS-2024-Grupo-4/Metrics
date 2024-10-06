191f66316bce44526fb9cdd4e50c6c79e9c7b700
Hiogen <tkd55fernandezfiel@gmail.com>
2024-10-06 20:19:10 -0300

    Metrics Actualizadas Vianda

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -38,7 +38,7 @@ public class WebApp {
         app.get("/metrics/cantDonadores", metricController::cantDonadores);
         app.get("/metrics/cantTransportadores", metricController::cantTransportadores);
         app.get("/metrics/viandasCreadas", metricController::viandasCreadas);
-        app.get("/metrics/viandasDepositadas", metricController::viandasDepositadas);
+        app.get("/metrics/viandasEnTransporte", metricController::viandasEnTransporte);
         app.get("/metrics/viandasVencidas", metricController::viandasVencidas);
     }
 
--- src/main/java/ar/edu/utn/dds/k3003/controller/MetricController.java
+++ src/main/java/ar/edu/utn/dds/k3003/controller/MetricController.java
@@ -3,14 +3,24 @@ package ar.edu.utn.dds.k3003.controller;
 import ar.edu.utn.dds.k3003.service.DDMetricsUtils;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
+import io.micrometer.core.instrument.Gauge;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.AbstractMap;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 public class MetricController {
   private final DDMetricsUtils metricsUtils;
+  private final AtomicInteger trasladosEnCurso = new AtomicInteger(0);
+  private final AtomicInteger cantColaboradores = new AtomicInteger(0);
+  private final AtomicInteger cantDonadores = new AtomicInteger(0);
+  private final AtomicInteger cantTransportadores = new AtomicInteger(0);
+  private final AtomicInteger viandasCreadas = new AtomicInteger(0);
+  private final AtomicInteger viandasDepositadas = new AtomicInteger(0);
+  private final AtomicInteger viandasVencidas = new AtomicInteger(0);
 
   public MetricController(DDMetricsUtils metricsUtils) {
     this.metricsUtils = metricsUtils;
@@ -18,7 +28,7 @@ public class MetricController {
   public void aperturaHeladera(Context context) {
     try {
       String heladeraIdParam = context.pathParam("heladeraId");
-      metricsUtils.getRegistry().counter("heladera"+ heladeraIdParam + ".aperturas").increment();
+      metricsUtils.getRegistry().counter("heladerasAperturas").increment();
       context.status(HttpStatus.OK);
       log.info("Apertura de heladera registrada.");
     } catch (Exception e) {
@@ -26,10 +36,30 @@ public class MetricController {
       log.error("Error al registrar la apertura de la heladera", e);
     }
   }
-
+  public void excesoTemperaturaHeladera(Context context){
+    try{
+    String heladeraId = context.pathParam("heladeraId");
+    metricsUtils.getRegistry().counter("excesoTemperaturaHeladera"+heladeraId).increment();
+    context.status(HttpStatus.OK);
+    log.info("Exceso de temperatura registrada.");
+    } catch (Exception e) {
+    context.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    log.error("Error al registrar un exceso de temperatura en la heladera", e);
+    }
+  }
+  public void consultaHeladera(Context context) {
+    try {
+      metricsUtils.getRegistry().counter("consultaHeladeras").increment();
+      context.status(HttpStatus.OK);
+      log.info("Consulta de heladera registrada.");
+    } catch (Exception e) {
+      context.status(HttpStatus.INTERNAL_SERVER_ERROR);
+      log.error("Error al registrar la consulta de la heladera", e);
+    }
+  }
   public void trasladosRealizados(Context context) {
     try {
-      metricsUtils.getRegistry().counter("traslado.finalizado").increment();
+      metricsUtils.getRegistry().counter("trasladoFinalizado").increment();
       context.status(HttpStatus.OK);
       log.info("Nuevo traslado finalizado");
     } catch (Exception e) {
@@ -43,10 +73,10 @@ public class MetricController {
       String accion = context.queryParam("accion");
 
       if ("incrementar".equals(accion)) {
-        metricsUtils.getRegistry().gauge("trasladosEnCurso", new AtomicInteger(0)).incrementAndGet();
+        metricsUtils.getRegistry().gauge("trasladosEnCurso", trasladosEnCurso.incrementAndGet());
         log.info("Traslados en curso incrementados.");
       } else if ("disminuir".equals(accion)) {
-        metricsUtils.getRegistry().gauge("trasladosEnCurso", new AtomicInteger(0)).decrementAndGet();
+        metricsUtils.getRegistry().gauge("trasladosEnCurso", trasladosEnCurso.decrementAndGet());
         log.info("Traslados en curso disminuidos.");
       } else {
         throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
@@ -58,34 +88,36 @@ public class MetricController {
     }
   }
 
-  public void colaboradorConMasPuntos(Context context) {
-    try {
-      String colaboradorIdParam = context.pathParam("colaboradorId");
-      AtomicInteger MaxPuntos= metricsUtils.getRegistry().gauge("MaxPuntos",new AtomicInteger(0));
-      AtomicInteger colaboradorConMasPuntos= metricsUtils.getRegistry().gauge("colaboradorConMasPuntos",new AtomicInteger(0));
-      int puntos = Integer.parseInt(context.queryParam("puntos"));
-      if(puntos>MaxPuntos.get())
-      {
-        MaxPuntos.set(puntos);
-        colaboradorConMasPuntos.set(Integer.parseInt(context.queryParam("colaboradorConMasPuntos")));
-        log.info("Nuevo colaborador con más puntos!");
+  public void CantColaboradores(Context context) {
+      try {
+          String accion = context.pathParamAsClass("accion", String.class).get();
+
+          if ("incrementar".equals(accion)) {
+              metricsUtils.getRegistry().gauge("cantColaboradores", cantColaboradores.incrementAndGet());
+              log.info("cantColaboradores incrementados.");
+          } else if ("disminuir".equals(accion)) {
+              metricsUtils.getRegistry().gauge("cantColaboradores", cantColaboradores.decrementAndGet());
+              log.info("cantColaboradores disminuidos.");
+          } else {
+              throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
+          }
+          context.status(HttpStatus.OK);
+          log.info("Nuevo colaborador agregado");
+      } catch (Exception e) {
+          context.status(HttpStatus.INTERNAL_SERVER_ERROR);
+          log.error("Error al agregar colaborador", e);
       }
-      context.status(HttpStatus.OK);
-    } catch (Exception e) {
-      context.status(HttpStatus.INTERNAL_SERVER_ERROR);
-      log.error("Error al cambiar el valor", e);
-    }
   }
 
   public void cantDonadores(Context context) {
     try {
-      String accion = context.queryParam("accion");
+        String accion = context.pathParamAsClass("accion", String.class).get();
 
       if ("incrementar".equals(accion)) {
-        metricsUtils.getRegistry().gauge("cantDonadores", new AtomicInteger(0)).incrementAndGet();
+        metricsUtils.getRegistry().gauge("cantDonadores", cantDonadores.incrementAndGet());
         log.info("cantDonadores incrementados.");
       } else if ("disminuir".equals(accion)) {
-        metricsUtils.getRegistry().gauge("cantDonadores", new AtomicInteger(0)).decrementAndGet();
+        metricsUtils.getRegistry().gauge("cantDonadores", cantDonadores.decrementAndGet());
         log.info("cantDonadores disminuidos.");
       } else {
         throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
@@ -99,13 +131,13 @@ public class MetricController {
 
   public void cantTransportadores(Context context) {
     try {
-      String accion = context.queryParam("accion");
+        String accion = context.pathParamAsClass("accion", String.class).get();
 
-      if ("incrementar".equals(accion)) {
-        metricsUtils.getRegistry().gauge("cantTransportadores", new AtomicInteger(0)).incrementAndGet();
+        if ("incrementar".equals(accion)) {
+        metricsUtils.getRegistry().gauge("cantTransportadores", cantTransportadores.incrementAndGet());
         log.info("cantidad de transportadores incrementados.");
       } else if ("disminuir".equals(accion)) {
-        metricsUtils.getRegistry().gauge("cantTransportadores", new AtomicInteger(0)).decrementAndGet();
+        metricsUtils.getRegistry().gauge("cantTransportadores", cantTransportadores.decrementAndGet());
         log.info("cantidad de transportadores disminuidos.");
       } else {
         throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
@@ -124,35 +156,32 @@ public class MetricController {
 	      if ("incrementar".equals(accion)) {
 	        metricsUtils.getRegistry().gauge("viandasCreadas", new AtomicInteger(0)).incrementAndGet();
 	        log.info("viandasCreadas aumento.");
-	      } else if ("disminuir".equals(accion)) {
-	        metricsUtils.getRegistry().gauge("viandasCreadas", new AtomicInteger(0)).decrementAndGet();
-	        log.info("viandasCreadas disminuyo.");
 	      } else {
 	        throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
 	      }
 	      context.status(HttpStatus.OK);
 	    } catch (Exception e) {
 	      context.status(HttpStatus.INTERNAL_SERVER_ERROR);
-	      log.error("Error al actualizar la cantidad de viandas", e);
+	      log.error("Error al actualizar la cantidad de viandas creadas", e);
 	    }
 	  }
-  public void viandasDepositadas(Context context) {
+  public void viandasEnTransporte(Context context) {
 	  try {
 	      String accion = context.queryParam("accion");
 
-	      if ("viandasDepositadas".equals(accion)) {
-	        metricsUtils.getRegistry().gauge("viandasDepositadas", new AtomicInteger(0)).incrementAndGet();
-	        log.info("viandasDepositadas aumento.");
+	      if ("viandasEnTransporte".equals(accion)) {
+	        metricsUtils.getRegistry().gauge("viandasEnTransporte", new AtomicInteger(0)).incrementAndGet();
+	        log.info("viandasEnTransporte aumento.");
 	      } else if ("disminuir".equals(accion)) {
-	        metricsUtils.getRegistry().gauge("viandasDepositadas", new AtomicInteger(0)).decrementAndGet();
-	        log.info("viandasDepositadas disminuyo.");
+	        metricsUtils.getRegistry().gauge("viandasEnTransporte", new AtomicInteger(0)).decrementAndGet();
+	        log.info("viandasEnTransporte disminuyo.");
 	      } else {
 	        throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
 	      }
 	      context.status(HttpStatus.OK);
 	    } catch (Exception e) {
 	      context.status(HttpStatus.INTERNAL_SERVER_ERROR);
-	      log.error("Error al actualizar la cantidad de viandas", e);
+	      log.error("Error al actualizar la cantidad de viandas en transporte", e);
 	    }
 	  }
   public void viandasVencidas(Context context) {
@@ -162,16 +191,13 @@ public class MetricController {
 	      if ("incrementar".equals(accion)) {
 	        metricsUtils.getRegistry().gauge("viandasVencidas", new AtomicInteger(0)).incrementAndGet();
 	        log.info("viandasVencidas aumento.");
-	      } else if ("disminuir".equals(accion)) {
-	        metricsUtils.getRegistry().gauge("viandasVencidas", new AtomicInteger(0)).decrementAndGet();
-	        log.info("viandasVencidas disminuyo.");
 	      } else {
 	        throw new IllegalArgumentException("Acción no válida. Debe ser 'incrementar' o 'disminuir'.");
 	      }
 	      context.status(HttpStatus.OK);
 	    } catch (Exception e) {
 	      context.status(HttpStatus.INTERNAL_SERVER_ERROR);
-	      log.error("Error al actualizar la cantidad de viandas", e);
+	      log.error("Error al actualizar la cantidad de viandas vencidas", e);
 	    }
 	  }
 }
